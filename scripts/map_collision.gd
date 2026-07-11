8379035a4e0428ec27f205ab12e4d8d63b2a324b
Lance Phung <lncphung@gmail.com>
2026-07-11 13:05:35 -0400

    Added height map collision

---
+++ scripts/map_collision.gd
@@ -0,0 +1,15 @@
+extends CollisionShape3D
+
+
+func _ready():
+	var heightmap = HeightMapShape3D.new()
+
+	var image = MapData.height.get_image()
+
+	image.convert(Image.Format.FORMAT_RF)
+
+	heightmap.update_map_data_from_image(image, 0, 1)
+
+	shape = heightmap
+
+	scale = Vector3(MapData.WORLD_SIZE / MapData.RESOLUTION, 10, MapData.WORLD_SIZE / MapData.RESOLUTION)
