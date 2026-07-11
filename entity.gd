a7e2654458449e49e349e38d0f2f250f4a171b5c
Lance Phung <lncphung@gmail.com>
2026-07-11 12:45:52 -0400

    Added entity height stuff

--- entity.gd
+++ entity.gd
@@ -5,9 +5,12 @@ var pos: Vector2
 @onready var geometry = %Geometry
 
 func update_height():
-	var height = MapData.height.get_image().get_pixelv(pos.round()).r
+	var height = MapData.height.get_image().get_pixelv(pos.round().clamp(Vector2.ZERO, Vector2.ONE * (MapData.RESOLUTION - 1))).r
 
-	position = Vector3(pos.x, height, pos.y)
+	position = Vector3(pos.x * MapData.WORLD_SIZE / MapData.RESOLUTION - MapData.WORLD_SIZE / 2, height * 10, pos.y * MapData.WORLD_SIZE / MapData.RESOLUTION - MapData.WORLD_SIZE / 2)
 
 func _process(delta):
-	pass
+	pos.x = fmod(pos.x + delta * 20, MapData.RESOLUTION)
+	pos.y = fmod(pos.y + delta * 10, MapData.RESOLUTION)
+
+	update_height()
